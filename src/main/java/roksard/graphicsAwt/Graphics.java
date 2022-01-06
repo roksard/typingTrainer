915d68256df0b451ef6e3538b941f378b18ad16e
roksard <roksard@gmail.com>
2022-01-06 21:59:46 +0100

    add drawRect in Graphics

--- src/main/java/roksard/graphicsAwt/Graphics.java
+++ src/main/java/roksard/graphicsAwt/Graphics.java
@@ -2,6 +2,7 @@ package roksard.graphicsAwt;
 
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
 
 public class Graphics {
     public void drawCircle(java.awt.Graphics g, double x, double y, double size, Color color) {
@@ -10,5 +11,13 @@ public class Graphics {
         Ellipse2D.Double circle = new Ellipse2D.Double(x * s, y * s, size, size);
         g2d.setColor(color);
         g2d.fill(circle);
+    }
+
+    public void drawRect(java.awt.Graphics g, double x, double y, double width, double height, Color color) {
+        double s = 1; //some scale for coorinates
+        Graphics2D g2d = (Graphics2D) g;
+        Rectangle2D.Double rect = new Rectangle2D.Double(x * s, y * s, width, height);
+        g2d.setColor(color);
+        g2d.fill(rect);
     }
 }
