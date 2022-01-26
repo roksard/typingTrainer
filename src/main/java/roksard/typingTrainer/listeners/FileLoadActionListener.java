5f2f3035fa2a305239eb24ce434c6b02e1ec3a8b
roksard <roksard@gmail.com>
2022-01-26 18:41:43 +0100

    dont load file if previous load is not yet finished

--- src/main/java/roksard/typingTrainer/listeners/FileLoadActionListener.java
+++ src/main/java/roksard/typingTrainer/listeners/FileLoadActionListener.java
@@ -18,6 +18,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 @Setter
 @Getter
@@ -31,6 +32,7 @@ public class FileLoadActionListener implements ActionListener {
     final int posInChunk = chunkSize * 50 / 100; //at 50% of chunk
     private File currentFile;
     Logger logger = LogManager.getLogger(this.getClass());
+    private Future<?> loadFile;
 
     public FileLoadActionListener(JFrame frame, JTextArea epText, Config config, ExecutorService executorService) {
         this.frame = frame;
@@ -40,41 +42,47 @@ public class FileLoadActionListener implements ActionListener {
     }
 
     public void loadFile(File file, long filePos) {
-        logger.debug(">>load file at pos: {} ", filePos);
-        executorService.submit(new Runnable() {
-            @Override
-            public void run() {
-                long seekPos = Math.max(0, filePos - posInChunk);
-                if (currentFile == file && getSeekPos() == 0 && seekPos == 0) {
-                    logger.debug("trying to open file at 0 pos twice");
-                    return; //trying to open same file at same pos
-                }
-                if (currentFile == file && getSeekPos() + chunkSize >= file.length() && seekPos >= getSeekPos()) {
-                    logger.debug("trying to open file at EOF twice");
-                    return; //trying to open same file at same pos (eof)
-                }
-                CharBuffer charBuffer;
-                try (
-                        FileChannel fileChannel = FileChannel.open(Paths.get(file.getAbsolutePath()));
-                ) {
-                    fileChannel.position(seekPos);
-                    ByteBuffer bb = ByteBuffer.allocate(chunkSize);
-                    int read = fileChannel.read(bb);
-                    bb.limit(read);
-                    bb.rewind();
-                    charBuffer = StandardCharsets.UTF_8.decode(bb);
-                } catch (Exception e) {
-                    logger.error("Error: ", e);
-                    throw new RuntimeException(e);
-                }
-                currentFile = file;
-                epText.setText(charBuffer.toString());
-                epText.setCaretPosition(calcCaretPosOffsetByBytes(epText.getText(), (int)(filePos - seekPos)));
-                epText.getCaret().setVisible(true);
-                config.setFileName(file.getAbsolutePath());
-                setSeekPos(seekPos);
+        synchronized (this) {
+            if (loadFile != null && !loadFile.isDone()) {
+                logger.debug("load file interrupted, because already running");
+                return;
             }
-        });
+            loadFile = executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    logger.debug(">>load file at pos: {} ", filePos);
+                    long seekPos = Math.max(0, filePos - posInChunk);
+                    if (currentFile == file && getSeekPos() == 0 && seekPos == 0) {
+                        logger.debug("trying to open file at 0 pos twice");
+                        return; //trying to open same file at same pos
+                    }
+                    if (currentFile == file && getSeekPos() + chunkSize >= file.length() && seekPos >= getSeekPos()) {
+                        logger.debug("trying to open file at EOF twice");
+                        return; //trying to open same file at same pos (eof)
+                    }
+                    CharBuffer charBuffer;
+                    try (
+                            FileChannel fileChannel = FileChannel.open(Paths.get(file.getAbsolutePath()));
+                    ) {
+                        fileChannel.position(seekPos);
+                        ByteBuffer bb = ByteBuffer.allocate(chunkSize);
+                        int read = fileChannel.read(bb);
+                        bb.limit(read);
+                        bb.rewind();
+                        charBuffer = StandardCharsets.UTF_8.decode(bb);
+                    } catch (Exception e) {
+                        logger.error("Error: ", e);
+                        throw new RuntimeException(e);
+                    }
+                    currentFile = file;
+                    epText.setText(charBuffer.toString());
+                    epText.setCaretPosition(calcCaretPosOffsetByBytes(epText.getText(), (int) (filePos - seekPos)));
+                    epText.getCaret().setVisible(true);
+                    config.setFileName(file.getAbsolutePath());
+                    setSeekPos(seekPos);
+                }
+            });
+        }
     }
 
     public int calcCaretPosOffsetByBytes(String text, int byteOffset) {
