94e364788f6a828b250ca547f719e7e804757795
roksard <roksard@gmail.com>
2022-08-26 20:51:56 +0200

    autoscroll text area when cursor hits last visible line

--- src/main/java/roksard/typingTrainer/listeners/EpTextCaretListener.java
+++ src/main/java/roksard/typingTrainer/listeners/EpTextCaretListener.java
@@ -8,11 +8,15 @@ import org.springframework.stereotype.Component;
 import javax.swing.*;
 import javax.swing.event.CaretEvent;
 import javax.swing.event.CaretListener;
+import javax.swing.text.BadLocationException;
+import java.awt.*;
 
 @Component
 public class EpTextCaretListener implements CaretListener {
     @Autowired
     JTextArea epText;
+    @Autowired
+    JScrollPane epTextScrollPane;
     @Autowired
     FileLoadActionListener fileLoadActionListener;
 
@@ -20,6 +24,19 @@ public class EpTextCaretListener implements CaretListener {
 
     @Override
     public void caretUpdate(CaretEvent e) {
+        try {
+            double caretYPos = epText.modelToView(epText.getCaretPosition()).getY();
+            Rectangle visibleRect = epText.getVisibleRect();
+            if ((caretYPos + (epText.getFontMetrics(epText.getFont()).getHeight()*2))
+                    > (visibleRect.getY() + visibleRect.getHeight())
+            ) {
+                JScrollBar scrollBar = epTextScrollPane.getVerticalScrollBar();
+                scrollBar.setValue(scrollBar.getValue() + (int)Math.round(visibleRect.getHeight()));
+            }
+        } catch (BadLocationException badLocationException) {
+            logger.error("Could not convert caret pos into XY view coordinate", badLocationException);
+        }
+
         double relativePos = epText.getCaretPosition() / (double)epText.getText().length();
         if (relativePos < 0.1 || relativePos > 0.9 ) {
             fileLoadActionListener.loadFile(fileLoadActionListener.getCurrentFile(), fileLoadActionListener.calcFilePosByCurrentPosAndCaretPos());
